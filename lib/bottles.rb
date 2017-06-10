bbb0871e0f9383fe81a1d1cebee0085ada57bfb2
Tony Baines <tony.baines.1@gmail.com>
2017-06-10 12:56:58 +0100

    99 bottles mark1 after tidy-up

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,25 +5,27 @@ class Bottles
   end
 
   def verses(v_from, v_to)
-    v_from.step(v_to, -1).collect{ |v|
+    v_from.step(v_to, -1).collect {|v|
       verse(v)
     }.join "\n"
   end
 
   def verse(v_num)
     if v_num == 0
-    "No more bottles of beer on the wall, no more bottles of beer.
+      'No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
-"
+'
     else
-    "#{v_num} #{bottle(v_num)} of beer on the wall, #{v_num} #{bottle(v_num)} of beer.
-Take #{(v_num-1 == 0) ? 'it' : 'one'} down and pass it around, #{(v_num-1 == 0) ? 'no more' : v_num-1} #{bottle(v_num-1)} of beer on the wall.
+      next_num = v_num-1
+      "#{v_num} #{v_num.to_bottle_s} of beer on the wall, #{v_num} #{v_num.to_bottle_s} of beer.
+Take #{(next_num == 0) ? 'it' : 'one'} down and pass it around, #{(next_num == 0) ? 'no more' : next_num} #{next_num.to_bottle_s} of beer on the wall.
 "
-      end
+    end
   end
+end
 
-  def bottle(count)
-    (count == 1) ? 'bottle' : 'bottles'
+class Fixnum
+  def to_bottle_s
+    (self == 1) ? 'bottle' : 'bottles'
   end
-
 end
